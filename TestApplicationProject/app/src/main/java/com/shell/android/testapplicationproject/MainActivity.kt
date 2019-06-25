420eeff6ef90935534f26e21769652727572eb75
Cesar Morales Garduño <shell.one.core@gmail.com>
2019-06-25 14:07:03 -0500

    Se agregó barra de menú lateral en la aplicación

--- TestApplicationProject/app/src/main/java/com/shell/android/testapplicationproject/MainActivity.kt
+++ TestApplicationProject/app/src/main/java/com/shell/android/testapplicationproject/MainActivity.kt
@@ -2,6 +2,7 @@ package com.shell.android.testapplicationproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
 import com.shell.android.baselibrary.basewidgets.BaseToolbarImpl
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,5 +14,15 @@ class MainActivity : BaseToolbarImpl() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         loadToolbar(mainToolbar as Toolbar)
+        setNavigationDrawer()
+    }
+
+    private fun setNavigationDrawer() {
+        val toogle = ActionBarDrawerToggle(this, mainContainer, R.string.nav_drawer_open, R.string.nav_drawer_close)
+        toogle.isDrawerIndicatorEnabled = true
+        mainContainer.addDrawerListener(toogle)
+        toogle.syncState()
+
+
     }
 }
