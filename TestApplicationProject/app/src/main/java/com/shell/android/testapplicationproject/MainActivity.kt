907fc0ade8cd2afda4678748e9e42855b057c5b1
Cesar Morales Garduño <shell.one.core@gmail.com>
2019-06-25 13:35:00 -0500

    Se agregó toolbar personalizado desde la librería base

--- TestApplicationProject/app/src/main/java/com/shell/android/testapplicationproject/MainActivity.kt
+++ TestApplicationProject/app/src/main/java/com/shell/android/testapplicationproject/MainActivity.kt
@@ -2,11 +2,16 @@ package com.shell.android.testapplicationproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import com.shell.android.baselibrary.basewidgets.BaseToolbarImpl
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseToolbarImpl() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadToolbar(mainToolbar as Toolbar)
     }
 }
